ce6e2c86b577a00d96b091ed0f4604014c040f56
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-09 08:04:00 +0000

    [Upload]5-11,投稿一覧実装

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,4 +4,13 @@ class PostImage < ApplicationRecord
   # PostImageモデルでuser_idに関連付けされていてUserモデルを参照できる
   # belongs_toは１：NのN側なので関連したUserモデルを参照できる
   
+  def get_image
+    unless image.attached?
+    # 画像が設定されていない場合
+      file_path = Rails.root.join('app/assets/images/no_image.png')
+      # app/assets/imagesに格納されているno_image.pngを表示させる
+      image.attach(io: File.open(file_path), filename: 'default-image.png',content_type:'image/png')
+    end
+    image
+  end
 end
