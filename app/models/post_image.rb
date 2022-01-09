c76f6e1e676a73aabb32cb66389ffcb3c93fcb19
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-01-09 06:26:52 +0000

    [Upload]5-9,画像投稿機能ルーティング作成

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,6 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+  # PostImageモデルでuser_idに関連付けされていてUserモデルを参照できる
+  # belongs_toは１：NのN側なので関連したUserモデルを参照できる
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@ class User < ApplicationRecord
           # ログイン情報を保存
           :validatable
           # emailのフォーマットなどのバリデーション
+  has_many :post_images, dependent: :destroy
+  # Userモデルはpost_imageモデルをN個持っている（has_many :post_images）
+  # 1:Nの1側が削除された時、N側を全て削除する（dependent: :destroy）
 end
